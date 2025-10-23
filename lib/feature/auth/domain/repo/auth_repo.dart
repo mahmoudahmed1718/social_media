506a661b3fc3f2b9cc9055559887ad6813340bbc
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-10-23 17:19:40 +0300

    Refactor AuthRepo to use Either for login and register methods

--- lib/feature/auth/domain/repo/auth_repo.dart
+++ lib/feature/auth/domain/repo/auth_repo.dart
@@ -1,3 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:social_media/core/errors/server_faileur.dart';
+import 'package:social_media/feature/auth/domain/entites/app_user.dart';
+
 abstract class AuthRepo {
-  Future<void> signIn(String email, String password);
+  Future<Either<ServerFaileur, AppUser>> login({
+    required String email,
+    required String password,
+  });
+  Future<Either<ServerFaileur, AppUser>> register({
+    required String name,
+    required String email,
+    required String password,
+  });
+  Future<void> logout();
 }
