3e3eac3c1b53b129eb3ec3025761c3722c8d3095
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-10-23 18:01:30 +0300

    add sharedprefences package to store keys and values

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:social_media/core/services/get_it_service.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await SharedPreferences.getInstance();
+
+  await SharedPreferences.getInstance();
   setupGetItService();
   runApp(const MyApp());
 }
