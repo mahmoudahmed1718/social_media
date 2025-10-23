444f170e48503fcff9eb2927648f14515d732bb8
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-10-23 17:01:49 +0300

    create assets file to get images

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAppLogo
+  /// assets/images/app_logo.jpg
+  static const String imagesAppLogo = "assets/images/app_logo.jpg";
+}
+
