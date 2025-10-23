8ac65763ab4bceadece968353b14c5d61c8ab617
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-10-23 17:35:01 +0300

    Enhance FirebaseAuthService with improved error handling and messages for user creation and sign-in

--- lib/core/services/firebase_auth_service.dart
+++ lib/core/services/firebase_auth_service.dart
@@ -1,5 +1,83 @@
+import 'dart:developer';
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:social_media/core/errors/custom_excption.dart';
 
 class FirebaseAuthService {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<User?> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    try {
+      final credential = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential.user;
+    } on FirebaseAuthException catch (e) {
+      log(
+        'execption on firebaseAuthServerce.createUserWithEmailAndPassword. ${e.toString()}and code is ${e.code}',
+      );
+      if (e.code == 'weak-password') {
+        throw CustomException(message: 'The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        throw CustomException(
+          message: 'The account already exists for that email.',
+        );
+      } else if (e.code == 'network-request-failed') {
+        throw CustomException(
+          message: 'Please check your internet connection.',
+        );
+      } else {
+        throw CustomException(message: 'An unknown error occurred.');
+      }
+    } catch (e) {
+      log(
+        'execption on firebaseAuthServerce.createUserWithEmailAndPassword. ${e.toString()}',
+      );
+      throw CustomException(message: 'An unknown error occurred.');
+    }
+  }
+
+  Future<User?> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final credential = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential.user;
+    } on FirebaseAuthException catch (e) {
+      log(
+        'execption on firebaseAuthServerce.signInWithEmailAndPassword. ${e.toString()}and code is ${e.code}',
+      );
+      if (e.code == 'user-not-found') {
+        throw CustomException(message: 'No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        throw CustomException(
+          message: 'Wrong password provided for that user.',
+        );
+      } else if (e.code == 'network-request-failed') {
+        throw CustomException(
+          message: 'Please check your internet connection.',
+        );
+      } else {
+        throw CustomException(message: 'An unknown error occurred.');
+      }
+    } catch (e) {
+      log(
+        'execption on firebaseAuthServerce.signInWithEmailAndPassword. ${e.toString()}',
+      );
+      throw CustomException(message: 'An unknown error occurred.');
+    }
+  }
+
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
 }
