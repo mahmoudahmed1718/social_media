ca94560d85b5663d5cc754b49b47b27b7d8f42fb
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-10-23 17:47:01 +0300

    Refactor AppUserModel to use BackendPoint constants for JSON serialization and deserialization

--- lib/feature/auth/data/models/app_user_model.dart
+++ lib/feature/auth/data/models/app_user_model.dart
@@ -1,9 +1,22 @@
+import 'package:social_media/core/services/backend_point.dart';
 import 'package:social_media/feature/auth/domain/entites/app_user.dart';
 
 class AppUserModel extends AppUserEntity {
   AppUserModel({required super.uId, required super.name, required super.email});
 
   toJosn() {
-    return {'uId': uId, 'name': name, 'email': email};
+    return {
+      BackendPoint.uId: uId,
+      BackendPoint.name: name,
+      BackendPoint.email: email,
+    };
+  }
+
+  factory AppUserModel.fromJosn(Map<String, dynamic> json) {
+    return AppUserModel(
+      uId: json[BackendPoint.uId],
+      name: json[BackendPoint.name],
+      email: json[BackendPoint.email],
+    );
   }
 }
