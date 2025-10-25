ee39b9858e55d1aa2d09eea99c74ee15f07917f5
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-10-25 08:32:52 +0300

    implment database service and firestore service in get it sevice

--- lib/core/services/get_it_service.dart
+++ lib/core/services/get_it_service.dart
@@ -1,7 +1,10 @@
 import 'package:get_it/get_it.dart';
+import 'package:social_media/core/services/database_service.dart';
 import 'package:social_media/core/services/firebase_auth_service.dart';
+import 'package:social_media/core/services/firestore_service.dart';
 
 GetIt getIt = GetIt.instance;
 void setupGetItService() {
   getIt.registerSingleton<FirebaseAuthService>(FirebaseAuthService());
+  getIt.registerSingleton<DatabaseService>(FirestoreService());
 }
