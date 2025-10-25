764f4f4a9e34f40c1f24183ccac505850afd5364
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-10-25 12:04:07 +0300

    create styels file for text styel for all app

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  textStyle16(BuildContext context) =>
+      TextStyle(fontSize: 16, color: Theme.of(context).colorScheme.primary);
+}
