26cdfaa69e391b9f59cccfb00310244809acf985
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-10-25 11:02:41 +0300

    feat: add AuthCubit and AuthState for authentication management and flutter_bloc package

---
+++ lib/feature/auth/presentation/manger/auth/auth_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:social_media/feature/auth/domain/entites/app_user.dart';
+
+part 'auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit() : super(AuthInitial());
+}
