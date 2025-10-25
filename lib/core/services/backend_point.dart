64c5664d77f7f15224d4c4947f943293cd655e9e
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-10-25 08:27:14 +0300

    finsh function of add data

--- lib/core/services/backend_point.dart
+++ lib/core/services/backend_point.dart
@@ -1,4 +1,5 @@
 abstract class BackendPoint {
+  static const String users = 'users';
   static const String uId = 'uid';
   static const String name = 'name';
   static const String email = 'email';
--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -1,12 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:social_media/core/services/database_service.dart';
 
 class FirestoreService implements DatabaseService {
+  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
   @override
   Future<void> addDatatoDatabase({
     required String path,
     required Map<String, dynamic> data,
     String? documentId,
-  }) async {}
+  }) async {
+    if (documentId != null) {
+      await firebaseFirestore.collection(path).doc(documentId).set(data);
+    } else {
+      await firebaseFirestore.collection(path).add(data);
+    }
+  }
 
   @override
   Future<bool> checkDocumentExists({
