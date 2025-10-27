1e59ab31195db3780e87f71d86bbbcf0477a75e9
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-10-27 10:06:33 +0300

    add auth repo impl to getit service file

--- lib/core/services/get_it_service.dart
+++ lib/core/services/get_it_service.dart
@@ -2,9 +2,17 @@ import 'package:get_it/get_it.dart';
 import 'package:social_media/core/services/database_service.dart';
 import 'package:social_media/core/services/firebase_auth_service.dart';
 import 'package:social_media/core/services/firestore_service.dart';
+import 'package:social_media/feature/auth/data/repo_impl/auth_repo_impl.dart';
+import 'package:social_media/feature/auth/domain/repo/auth_repo.dart';
 
 GetIt getIt = GetIt.instance;
 void setupGetItService() {
   getIt.registerSingleton<FirebaseAuthService>(FirebaseAuthService());
   getIt.registerSingleton<DatabaseService>(FirestoreService());
+  getIt.registerSingleton<AuthRepo>(
+    AuthRepoImpl(
+      firebaseAuthService: getIt.get<FirebaseAuthService>(),
+      databaseService: getIt.get<FirestoreService>(),
+    ),
+  );
 }
--- lib/feature/auth/presentation/views/login_view.dart
+++ lib/feature/auth/presentation/views/login_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_media/core/services/get_it_service.dart';
+import 'package:social_media/feature/auth/presentation/manger/auth/auth_cubit.dart';
 import 'package:social_media/feature/auth/presentation/views/widgets/login_view_body_bloc_consumer.dart';
 
 class LoginView extends StatelessWidget {
@@ -6,6 +9,13 @@ class LoginView extends StatelessWidget {
   static const String routeName = 'login_view';
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(body: LoginViewBodyBlocConsumer()));
+    return SafeArea(
+      child: BlocProvider(
+        create: (context) => AuthCubit(repo: 
+        
+        ),
+        child: Scaffold(body: LoginViewBodyBlocConsumer()),
+      ),
+    );
   }
 }
