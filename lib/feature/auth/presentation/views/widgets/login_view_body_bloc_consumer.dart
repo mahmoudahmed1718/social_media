e8ca15c862c08294648cb9643ba22acae90482ad
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-10-27 09:56:31 +0300

    create snak bar method and add bloc consumer in login view body as bloc_integrate

--- lib/feature/auth/presentation/views/widgets/login_view_body_bloc_consumer.dart
+++ lib/feature/auth/presentation/views/widgets/login_view_body_bloc_consumer.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_media/core/helper/snak_bar_method.dart';
+import 'package:social_media/feature/auth/presentation/manger/auth/auth_cubit.dart';
 import 'package:social_media/feature/auth/presentation/views/widgets/login_view_body.dart';
 
 class LoginViewBodyBlocConsumer extends StatelessWidget {
@@ -6,6 +9,17 @@ class LoginViewBodyBlocConsumer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LoginViewBody();
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state is AuthSuccess) {
+          showSnakbar(context, 'Login Successfully');
+          // ignore: avoid_types_as_parameter_names
+          Navigator.pushNamedAndRemoveUntil(context, '', (route) => false);
+        }
+      },
+      builder: (context, state) {
+        return LoginViewBody();
+      },
+    );
   }
 }
