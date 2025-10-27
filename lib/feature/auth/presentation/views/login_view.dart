baf2bf2208e5b8dc0871dc5288fd896163adc3fc
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-10-27 09:45:22 +0300

    create login view body bloc consumer and refactor code

--- lib/feature/auth/presentation/views/login_view.dart
+++ lib/feature/auth/presentation/views/login_view.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:social_media/feature/auth/presentation/views/widgets/login_view_body.dart';
+import 'package:social_media/feature/auth/presentation/views/widgets/login_view_body_bloc_consumer.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
   static const String routeName = 'login_view';
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(body: LoginViewBody()));
+    return SafeArea(child: Scaffold(body: LoginViewBodyBlocConsumer()));
   }
 }
---
+++ lib/feature/auth/presentation/views/widgets/login_view_body_bloc_consumer.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:social_media/feature/auth/presentation/views/widgets/login_view_body.dart';
+
+class LoginViewBodyBlocConsumer extends StatelessWidget {
+  const LoginViewBodyBlocConsumer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LoginViewBody();
+  }
+}
