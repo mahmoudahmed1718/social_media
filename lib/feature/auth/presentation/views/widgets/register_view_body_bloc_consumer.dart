76d5ae29aef598d554e20b71524e373c3635ebe2
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-11-01 12:50:39 +0200

    interger bloc consumer to regsiter view body

--- lib/feature/auth/presentation/views/widgets/register_view_body_bloc_consumer.dart
+++ lib/feature/auth/presentation/views/widgets/register_view_body_bloc_consumer.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_media/core/helper/snak_bar_method.dart';
+import 'package:social_media/core/widget/modal_progress_hud_loading_widget.dart';
+import 'package:social_media/feature/auth/presentation/manger/auth/auth_cubit.dart';
 import 'package:social_media/feature/auth/presentation/views/widgets/register_view_body.dart';
 
 class RegisterViewBodyBlocConsumer extends StatelessWidget {
@@ -6,6 +10,20 @@ class RegisterViewBodyBlocConsumer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return RegisterViewBody();
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state is AuthSuccess) {
+          showSnakbar(context, 'Register Successfully');
+        } else if (state is AuthFailure) {
+          showSnakbar(context, state.errorMessage);
+        }
+      },
+      builder: (context, state) {
+        return ModalProgressHudIsLoadingWidget(
+          isLoading: state is AuthLoading,
+          child: RegisterViewBody(),
+        );
+      },
+    );
   }
 }
