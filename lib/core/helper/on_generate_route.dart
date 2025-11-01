62520a14f034b3e3de7d4460d5fe90adb5a59aac
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-11-01 13:29:25 +0200

    create home feature and add home view page to ongentrate route

--- lib/core/helper/on_generate_route.dart
+++ lib/core/helper/on_generate_route.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:social_media/feature/auth/presentation/views/login_view.dart';
 import 'package:social_media/feature/auth/presentation/views/register_view.dart';
+import 'package:social_media/feature/home/presentation/view/home_view.dart';
 import 'package:social_media/feature/splash/presentation/views/splash_view.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
@@ -13,6 +14,9 @@ Route<dynamic> onGenerateRoute(RouteSettings settings) {
 
     case RegisterView.routeName:
       return MaterialPageRoute(builder: (context) => const RegisterView());
+
+    case HomeView.routeName:
+      return MaterialPageRoute(builder: (context) => const HomeView());
 
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
--- lib/feature/auth/presentation/views/widgets/login_view_body.dart
+++ lib/feature/auth/presentation/views/widgets/login_view_body.dart
@@ -14,6 +14,10 @@ class LoginViewBody extends StatefulWidget {
 class _LoginViewBodyState extends State<LoginViewBody> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? email;
+  String? password;
   bool isObscure = true;
   @override
   void dispose() {
@@ -25,71 +29,74 @@ class _LoginViewBodyState extends State<LoginViewBody> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: ListView(
-        children: [
-          const SizedBox(height: 60),
-          Icon(
-            Icons.login,
-            size: 100,
-            color: Theme.of(context).colorScheme.primary,
-          ),
-          const SizedBox(height: 16),
-
-          Center(
-            child: Text(
-              'welcome Back...,you have been missed',
-              style: TextStyle(
-                fontSize: Styles.style16,
-                color: Theme.of(context).colorScheme.primary,
-              ),
+      child: Form(
+        key: formKey,
+        child: ListView(
+          children: [
+            const SizedBox(height: 60),
+            Icon(
+              Icons.login,
+              size: 100,
+              color: Theme.of(context).colorScheme.primary,
             ),
-          ),
-          const SizedBox(height: 20),
-          MyTextField(
-            controller: emailController,
-            hintText: 'Email',
-            icon: Icons.email,
-          ),
-          const SizedBox(height: 16),
-          MyTextField(
-            controller: passwordController,
-            hintText: 'Password',
-            obscureText: isObscure,
-            icon: isObscure ? Icons.visibility : Icons.visibility_off,
-            onPressed: () {
-              setState(() {
-                isObscure = !isObscure;
-              });
-            },
-          ),
-          const SizedBox(height: 24),
-          MyButton(text: 'Login', onTap: () {}),
-          const SizedBox(height: 24),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Not a member...?',
+            const SizedBox(height: 16),
+
+            Center(
+              child: Text(
+                'welcome Back...,you have been missed',
                 style: TextStyle(
                   fontSize: Styles.style16,
                   color: Theme.of(context).colorScheme.primary,
                 ),
               ),
-              TextButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, RegisterView.routeName);
-                },
-                child: Text(
-                  'Register now',
+            ),
+            const SizedBox(height: 20),
+            MyTextField(
+              controller: emailController,
+              hintText: 'Email',
+              icon: Icons.email,
+            ),
+            const SizedBox(height: 16),
+            MyTextField(
+              controller: passwordController,
+              hintText: 'Password',
+              obscureText: isObscure,
+              icon: isObscure ? Icons.visibility : Icons.visibility_off,
+              onPressed: () {
+                setState(() {
+                  isObscure = !isObscure;
+                });
+              },
+            ),
+            const SizedBox(height: 24),
+            MyButton(text: 'Login', onTap: () {}),
+            const SizedBox(height: 24),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Not a member...?',
                   style: TextStyle(
                     fontSize: Styles.style16,
-                    color: Theme.of(context).colorScheme.inversePrimary,
+                    color: Theme.of(context).colorScheme.primary,
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, RegisterView.routeName);
+                  },
+                  child: Text(
+                    'Register now',
+                    style: TextStyle(
+                      fontSize: Styles.style16,
+                      color: Theme.of(context).colorScheme.inversePrimary,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
