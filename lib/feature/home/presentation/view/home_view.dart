cdbc848973839478e1d9d4ba14d9ec643e3d661c
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-11-01 13:40:01 +0200

    create home view body

--- lib/feature/home/presentation/view/home_view.dart
+++ lib/feature/home/presentation/view/home_view.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:social_media/feature/home/presentation/view/widgets/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
   static const String routeName = 'home_view';
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home View')),
+      body: HomeViewBody(),
+    );
   }
 }
---
+++ lib/feature/home/presentation/view/widgets/home_view_body.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
