455a9377f32b6b36388294fc14e658206ba31843
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-11-02 08:28:51 +0200

    feat: add user profile fields to BackendPoint class

--- lib/core/services/backend_point.dart
+++ lib/core/services/backend_point.dart
@@ -3,4 +3,7 @@ abstract class BackendPoint {
   static const String uId = 'uid';
   static const String name = 'name';
   static const String email = 'email';
+  static const String username = 'username';
+  static const String bio = 'bio';
+  static const String profilePictureUrl = 'profilePictureUrl';
 }
