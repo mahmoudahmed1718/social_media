4daf4c09824a073ecd1846306fa9cf50d1349c26
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-11-02 08:32:58 +0200

    create profile user model file and method of convert to json and from json

---
+++ lib/feature/profile/data/models/profile_user_model.dart
@@ -0,0 +1,36 @@
+import 'package:social_media/core/services/backend_point.dart';
+import 'package:social_media/feature/profile/domain/entities/profile_user_entity.dart';
+
+class ProfileUserModel extends ProfileUserEntity {
+  ProfileUserModel({
+    super.username,
+    super.bio,
+    super.profilePictureUrl,
+    required super.uId,
+    required super.name,
+    required super.email,
+  });
+  // convert appuser to json
+  Map<String, dynamic> toJson() {
+    return {
+      BackendPoint.uId: uId,
+      BackendPoint.name: name,
+      BackendPoint.email: email,
+      BackendPoint.username: username,
+      BackendPoint.bio: bio,
+      BackendPoint.profilePictureUrl: profilePictureUrl,
+    };
+  }
+
+  // convert json to appuser
+  factory ProfileUserModel.fromJson(Map<String, dynamic> json) {
+    return ProfileUserModel(
+      uId: json[BackendPoint.uId],
+      name: json[BackendPoint.name],
+      email: json[BackendPoint.email],
+      username: json[BackendPoint.username],
+      bio: json[BackendPoint.bio],
+      profilePictureUrl: json[BackendPoint.profilePictureUrl],
+    );
+  }
+}
